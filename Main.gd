724e32f9b80b69c297a3f916c7e14e242ee6a081
ryancheung <ryancheung.go@gmail.com>
2019-01-09 21:17:09 +0800

    Update GDNative script

--- Main.gd
+++ Main.gd
@@ -8,7 +8,9 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	var simple = load("res://simple.gdns").new()
-	simple.say(simple.hello("Godot World"))
+	var msg = simple.hello("Godot World")
+	simple.say(msg)
+	self.text = msg
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
